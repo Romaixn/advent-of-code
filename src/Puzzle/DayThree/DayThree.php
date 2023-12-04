9ecda5808bc7300a5f4dd5ff9e6c04b6e9d6fcca
Romain Herault <romain.herault@les-tilleuls.coop>
2023-12-04 11:53:55 +0100

    feat: begin day 3

---
+++ src/Puzzle/DayThree/DayThree.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Puzzle\DayThree;
+
+use App\Puzzle\Day;
+
+final class DayThree extends Day
+{
+    public function partOne(): string
+    {
+        return '';
+    }
+
+    public function partTwo(): string
+    {
+        return '';
+    }
+}
